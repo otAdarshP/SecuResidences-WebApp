9c17a1c888200eedff138c30d73b6ea361dcf112
Adarsh Priyadarshi <adarshp0808@gmail.com>
2025-05-27 14:20:00 +0530

    netlify updations

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: './',
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // this must be here
+  base: './',
   plugins: [react()],
 })
+
+
